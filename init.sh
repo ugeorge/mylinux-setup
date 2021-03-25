69c9f5f31ea4ee1a106dc8eb86cd34d10af2e7e6
egeoung <george.ungureanu@ericsson.com>
2021-03-25 14:05:56 +0100

    debugged scripts

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-#!bin/bash
+#!/bin/bash
 
 # Emacs initial config
+mkdir -p ~/.emacs.d
 if [[ ! -e ~/.emacs.d/init.el ]]; then
-    mkdir -p ~/.emacs.d
     echo "(add-to-list 'load-path \"$(pwd)/emacs.d\")
-(load \"my-init\")" > ~/.emacs.d
+(load \"my-init\")" > ~/.emacs.d/init.el
 fi
 
 touch ~/run_env 
 if ! cmp ~/run_env docker/run_env >/dev/null 2>&1; then
-    mv docker/run_env ~/run_env
+    cp docker/run_env ~/run_env
 fi
